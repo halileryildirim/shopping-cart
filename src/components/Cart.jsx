cefb50e2f815b5aa843d51eb84711835a69dc47e
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-18 00:10:06 +0300

    Add Cart.css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { useCart } from "./CartContext";
 
 const Cart = () => {
-    const { cart, removeFromCart, totalPrice } = useCart();
+    const { cart, totalPrice, removeFromCart} = useCart();
 
     return (
         <>
@@ -10,15 +10,15 @@ const Cart = () => {
             <div>
                 <h2>Your Cart</h2>
                 {cart.map((item) => (
-                    <div key={item.id}>
-                        <img src={item.image} alt="item" />
-                        <p>{item.title}</p>
-                        <p>{item.quantity}</p>
-                        <p>Price: {item.price}</p>
-                        <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                    <div className="added-item" key={item.id}>
+                        <img className="added-item-img" src={item.image} alt="item" />
+                        <p className="added-item">{item.title}</p>
+                        <p className="added-item">{item.quantity}</p>
+                        <p className="added-item">Price: {item.price}</p>
+                        <button className="added-item" onClick={() => removeFromCart(item.id)}>Remove</button>
                     </div>
                 ))}
-                <p>Total Price: {totalPrice} $</p>
+                <p>{totalPrice === 0 ? "Your cart is empty" : `Total Price : ${totalPrice}$`}</p>
                 <button>Checkout</button>
             </div>
         </>
