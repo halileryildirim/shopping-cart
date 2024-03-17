a0376b2e620e959a794f4a7b4e6b9fb3669aeb52
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-03-18 02:01:50 +0300

    Refactor item quant

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -7,15 +7,23 @@ const AddToCart = ({ item }) => {
     const [quant, setQuant] = useState(1);
     const { addToCart } = useCart();
 
+
+    const onQuantChange = (e) => {
+        setQuant(e.target.value);
+    }
+
     const increaseQuant = () => {
-        if (quant < 10) {
-            setQuant((prevQuant) => prevQuant + 1);
+        if (quant === 0) {
+            setQuant(1)
+        }
+        else {
+            setQuant(quant + 1);
         }
     }
 
     const decreaseQuant = () => {
         if (quant > 1) {
-            setQuant((prevQuant)  => prevQuant - 1);
+            setQuant(quant - 1);
         }
     }
     
@@ -31,7 +39,7 @@ const AddToCart = ({ item }) => {
             <form className="cart-form">
                 <label htmlFor="add-input"></label>
                 <button type="button" className="count-button" onClick={decreaseQuant}>-</button>
-                <input type="number" id="add-input" value={quant} readOnly />
+                <input type="number" min={0} id="add-input" value={quant} onChange={onQuantChange} />
                 <button type="button" className="count-button" onClick={increaseQuant}>+</button>
                 <button type="button" onClick={handleCartSubmit} style={{ borderRadius: "4px", height: "32px", marginLeft: "12px" }}>Add to Cart</button>
             </form>
